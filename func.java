3044735013483d5847f5d0aa71ddd73a04212c0f
reddy887 <avulareddybabu@gmail.com>
2025-11-18 12:17:49 +0530

    dd arrays

---
+++ func.java
@@ -0,0 +1,13 @@
+public class func {
+    public static void main(String[] args){
+        int ans=add(2,5);
+        System.out.println(add(3,5));
+        System.out.println(ans);
+    }
+    public static int add(int a,int b){
+        int sum =a+b;
+        return sum;
+
+    }
+    
+}
