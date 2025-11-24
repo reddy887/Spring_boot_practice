f311a69c057d7f6e7646f03980947de0862564ca
reddy887 <avulareddybabu@gmail.com>
2025-11-24 15:00:15 +0530

    constructor  practice

---
+++ constructor.java
@@ -0,0 +1,14 @@
+public class constructor{
+    String name;
+    int age;
+    String course;
+    constructor(String n,int a,String c){
+        this.name=n;
+        this.age=a;
+        this.course=c;
+    }
+    public static void main(String[] args){
+        constructor con=new constructor("reddybabu",23,"bca");
+    }
+
+}
