249a8719aa43454f8f7bae6d4a5253391e0f4623
reddy887 <avulareddybabu@gmail.com>
2025-12-17 11:51:50 +0530

    constructor: Add default constructor and create an object using it

--- constructor.java
+++ constructor.java
@@ -7,9 +7,15 @@ public class constructor{
         this.age=a;
         this.course=c;
     }
+    constructor(){
+        System.out.println("default constructor");
+    }
     public static void main(String[] args){
         constructor con=new constructor("reddybabu",23,"bca");
         System.out.println(con.name);
+        constructor con1=new constructor();
+        con1.age=45;
+        con1.name="kumar";
     }
 
 }
